e3a5f1db908c533ec094a62009ec294659014a68
Jessie <Jessiesu3@gmail.com>
2022-09-16 16:46:21 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDom from "react-dom/client"
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const htmlContainer = document.getElementById("root");
+const root = ReactDom.createRoot(htmlContainer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function StudentInfo(props) {
+  return (
+    <div>
+      <div>
+        {props.lastName}, {props.firstName }
+      </div>
+      <ul>
+        <li>
+          <strong>ID:</strong> {props.sId}
+        </li>
+        <li>
+          <strong>School:</strong> {props.school}
+        </li>
+        <li>
+          <strong>Major:</strong> {props.major}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ClassList() {
+  return (
+    <div>
+      <h1 className ="red-text">Welcome to CTP</h1>
+      <p>List of Students</p>
+      <StudentInfo firstName="Sally" lastName="James" sId="123"/>
+      <StudentInfo firstName="Mavis" lastName="Amazing" sId="321"/>
+      <StudentInfo firstName="Peggie" lastName="Beautiful" sId="423"/>
+      <h1>Hello World!</h1>
+      <h1>Hi Again</h1>
+      {studentList.map((student) => (
+        <StudentInfo {...student} key={student.sId} />
+      ))}
+    </div>
+  );
+}
+
+const studentList = [
+  {
+    firstName: "Misty",
+    lastName: "Knight",
+    sId: "234",
+    school: "Queens College",
+    major: "Law",
+  },
+  {
+    firstName: "Jessica",
+    lastName: "Jones",
+    sId: "434",
+    school: "Brooklyn College",
+    major: "CS",
+  },
+  {
+    firstName: "Colleen",
+    lastName: "Wing",
+    sId: "233",
+    school: "Queens College",
+    major: "CS",
+  },
+  {
+    firstName: "Dare",
+    lastName: "Devil",
+    sId: "876",
+    school: "CCNY",
+    major: "Law",
+  },
+  {
+    firstName: "Luke",
+    lastName: "Cage",
+    sId: "323",
+    school: "CCNY",
+    major: "Math",
+  },
+];
+
+function CountApp() {
+  const [numClicks, setNumClicks] = useState(0);
+  const handleClick = (event) => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const test = Math.round(Math.random() * 255);
+  const test2 = Math.round(Math.random() * 255);
+  const test3 = Math.round(Math.random() * 255);
+  return (
+    <div>
+      <p>The button has been clicked {numClicks} times</p>
+
+      <button onClick={handleClick} style={ {backgroundColor: 'rgba(' + test + ',' + test2 + ',' + test3 + ',' + Math.random().toFixed(1) + ')'} }>Click this button</button>
+    </div>
+  );
+}
+
+root.render(<CountApp />);
